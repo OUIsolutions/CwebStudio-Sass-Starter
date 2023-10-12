be90f2191886f2fe5e67d8efc0854ff42a35d20c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-12 07:17:05 -0300

    at

--- src/database/users/users.c
+++ src/database/users/users.c
@@ -22,12 +22,9 @@ DtwResource *find_user_by_username_or_email(DtwResource  *database,const char *u
 
 bool  password_are_equal(DtwResource *user, char *entrie_passworld){
     DtwResource  *password = resource.sub_resource(user,PASSWORD_PATH);
-    char *password_str = resource.get_string(password);
-    char *password_sha = dtw.generate_sha_from_string(password_str);
-
+    char *password_sha = resource.get_string(password);
     char *entrie_sha = dtw.generate_sha_from_string(entrie_passworld);
     bool result = strcmp(password_sha,entrie_sha) == 0;
-    free(password_sha);
     free(entrie_sha);
     return  result;
 }
