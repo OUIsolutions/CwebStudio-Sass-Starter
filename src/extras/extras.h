90b1c27f14601e951bbad71cb8ca06975c404438
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-22 18:23:30 -0300

    at

--- src/extras/extras.h
+++ src/extras/extras.h
@@ -5,4 +5,4 @@ CwebHttpResponse  *send_chash_cleaning_memory(CHash *element,int status_code);
 CHashObject *join_headders_and_paramns(CwebHttpRequest *request);
 
 
-void aply_path_protection(CHashObject *element, char *key);
+void aply_path_protection(CHashObject *element,const  char *key);
