045c3b9fa7f4073ab3c5070eab176d77eb0de4ab
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-28 20:45:27 -0300

    at

--- add_route.py
+++ add_route.py
@@ -1,1 +1,16 @@
 
+
+
+def main():
+    
+    route_name = input("type the route name")
+    POSSIBLE_ROUTES = ['user','root','public']
+    type_route = input(f"type the type of route {POSSIBLE_ROUTES}")
+    
+    if  type_route not in POSSIBLE_ROUTES:
+        print(f"{type_route} not in {POSSIBLE_ROUTES}")
+        return
+    
+    
+
+main()
