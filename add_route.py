a30dcea3111e0d9d9000f0cff172622a3753d2d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-28 21:01:04 -0300

    att

--- add_route.py
+++ add_route.py
@@ -1,16 +1,22 @@
+USER = 'user'
+ROOT = 'root'
+PUBLIC = 'public'
+POSSIBLE_ROUTES = [USER,ROOT,PUBLIC]
 
 
+def add_route_constant(type):
+    
+    pass 
 
 def main():
     
     route_name = input("type the route name")
-    POSSIBLE_ROUTES = ['user','root','public']
     type_route = input(f"type the type of route {POSSIBLE_ROUTES}")
     
     if  type_route not in POSSIBLE_ROUTES:
         print(f"{type_route} not in {POSSIBLE_ROUTES}")
         return
     
-    
+
 
 main()
