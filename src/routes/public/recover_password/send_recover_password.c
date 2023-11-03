7a2420c34ef925e70ce89dd1658858d6cbba25ce
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-03 00:08:45 -0300

    at

--- src/routes/public/recover_password/send_recover_password.c
+++ src/routes/public/recover_password/send_recover_password.c
@@ -2,11 +2,26 @@
 
 CwebHttpResponse *public_recover_password_route(CwebHttpRequest *request, CHashObject*entries, DtwResource *database){
 
+    char *username_or_email = obj.getString(entries, LOGIN);
 
     CHash_catch(entries){
         return send_entrie_error(request, entries);
     }
-   
+
+    DtwResource *user = find_user_by_username_or_email(database,username_or_email);
+
+    if(!user){
+        UniversalGarbage_free(garbage);
+
+        return send_error(
+                request,
+                NOT_FOUND,
+                USER_NOT_EXIST,
+                USER_NOT_EXIST_MENSSAGE,
+                username_or_email
+        );
+    }
+    
     CHashObject *response = newCHashObject(
             CODE_KEY,hash.newNumber(INTERNAL_OK),
             MESSAGE_KEY,hash.newString("route ok")
