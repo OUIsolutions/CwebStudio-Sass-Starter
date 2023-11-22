412952a2bf474702c8170c1beb7d66f8d06ec6e6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-22 17:03:25 -0300

    att

--- src/api_bridge/api_bridge.c
+++ src/api_bridge/api_bridge.c
@@ -1,4 +1,18 @@
 
+
+ApiBridge *newApiBridge(){
+    ApiBridge *self = UniversalGarbage_create_empty_struct(self,ApiBridge);
+    self->garbage = newUniversalGarbage();
+    return  self;
+}
+
+void ApiBridge_set_token(ApiBridge *self,const char *token){
+    if(self->token){
+        free(self->token);
+    }
+    self->token = strdup(token);
+}
+
 void private_parse_chash_to_cweb_dict(CHash *value,CwebDict *target){
     if(!value){
         return;
@@ -39,6 +53,7 @@ CwebHttpResponse * ApiBridge_call_server_full(
         long content_size
 ){
     CwebHttpRequest * request = newCwebHttpRequest(-1);
+    request->route = strdup(route);
     private_parse_chash_to_cweb_dict(params,request->params);
     private_parse_chash_to_cweb_dict(headers,request->headers);
     if(content){
@@ -46,12 +61,37 @@ CwebHttpResponse * ApiBridge_call_server_full(
         request->content_length = content_size;
         request->content = content;
     }
-    return  main_sever(request);
 
+    if(self->token){
+        CwebDict_set(request->headers,TOKEN_ENTRE,self->token);
+    }
+
+    CwebHttpResponse*  response = main_sever(request);
+    UniversalGarbage_add(self->garbage, CwebHttpResponse_free,response);
+    return  response;
 }
+
 
 CwebHttpResponse * ApiBridge_call_server(ApiBridge*self, const char *route, CHash *entries){
     return ApiBridge_call_server_full(self, route, NULL, entries,NULL,NULL,-1);
 }
 
+CHash * ApiBridge_call_server_json(ApiBridge*self, const char *route, CHash *entries){
+    CwebHttpResponse *response = ApiBridge_call_server(self,route,entries);
+    if(!response){
+        return NULL;
+    }
+    CHash *response_hash = CHash_load_from_json_strimg((char*)response->content);
+    UniversalGarbage_add(self->garbage, CHash_free,response_hash);
+    return response_hash;
+}
 
+
+
+void ApiBridge_free(ApiBridge *self){
+    UniversalGarbage_free(self->garbage);
+    if(self->token){
+        free(self->token);
+    }
+    free(self);
+}
--- src/api_bridge/api_bridge.h
+++ src/api_bridge/api_bridge.h
@@ -1,12 +1,17 @@
 
 
+
 void private_parse_chash_to_cweb_dict(CHash *value,CwebDict *target);
 
 
 typedef struct {
-
+    char *token;
     UniversalGarbage *garbage;
 }ApiBridge;
+
+ApiBridge *newApiBridge();
+
+void ApiBridge_set_token(ApiBridge *self,const char *token);
 
 
 CwebHttpResponse * ApiBridge_call_server_full(
@@ -19,5 +24,10 @@ CwebHttpResponse * ApiBridge_call_server_full(
         long content_size
         );
 
+
 CwebHttpResponse * ApiBridge_call_server(ApiBridge*self, const char *route, CHash *entries);
 
+CHash * ApiBridge_call_server_json(ApiBridge*self, const char *route, CHash *entries);
+
+
+void ApiBridge_free(ApiBridge *self);
