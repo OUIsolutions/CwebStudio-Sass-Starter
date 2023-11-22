4490983350c165b6adfa296ee1133e3465b763b8
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-22 20:04:04 -0300

    att

---
+++ tests/main_test/create_token/T_invalid_password/exec.c
@@ -0,0 +1,22 @@
+
+#include "../../../../src/imports.h"
+#include "../../../../src/declaration.h"
+
+#undef DATABASE_PATH
+#define DATABASE_PATH "tests/target/content"
+#undef TRANSACTION_PATH
+#define TRANSACTION_PATH  "tests/target/transactions"
+
+#include "../../../../src/definition.h"
+
+
+int main(){
+    start_namespaces();
+    dtw_now = 0;
+
+    create_root_user_if_not_exist();
+    ApiBridge *bridge = newApiBridge();
+    ApiBridge_create_token(bridge,"root","invalid password",-1);
+    ApiBridge_represent(bridge);
+    ApiBridge_free(bridge);
+}
