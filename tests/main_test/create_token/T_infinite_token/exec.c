803c95c487e435634d8f1a43f472896cea5f6dc8
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-22 18:59:40 -0300

    at

--- tests/main_test/create_token/T_infinite_token/exec.c
+++ tests/main_test/create_token/T_infinite_token/exec.c
@@ -3,13 +3,14 @@
 #include "../../../../src/declaration.h"
 #undef DATABASE_PATH
 #define DATABASE_PATH "tests/target"
-#define API_TIME -1;
 
 #include "../../../../src/definition.h"
 
 
 int main(){
     start_namespaces();
+    #define API_TIME -1;
+
     create_root_user_if_not_exist();
     ApiBridge *bridge = newApiBridge();
     ApiBridge_create_token(bridge,"root","root",-1);
