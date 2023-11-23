cdb10b120ace1859ac34a34629daedbac11d1ba6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-22 22:13:36 -0300

    att

---
+++ tests/main_test/user/get_self_props/T_not_including_token/exec.c
@@ -0,0 +1,24 @@
+
+
+#include "../../../../../src/imports.h"
+#include "../../../../../src/declaration.h"
+
+#undef DATABASE_PATH
+#define DATABASE_PATH "tests/target/content"
+#undef TRANSACTION_PATH
+#define TRANSACTION_PATH  "tests/target/transactions"
+
+#include "../../../../../src/definition.h"
+
+
+int main(){
+    start_namespaces();
+    dtw_now = 0;
+
+    create_root_user_if_not_exist();
+    ApiBridge *bridge = newApiBridge();
+    ApiBridge_create_token(bridge,"root","root",-1);
+    CHash *props = ApiBridge_get_self_props(bridge,false,false);
+    ApiBridge_represent(bridge);
+    ApiBridge_free(bridge);
+}
