e80341dd0aa4560b38ac40624fcb7694ba8be69d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-22 21:59:31 -0300

    at

---
+++ tests/main_test/user/modify_self_props/T_user_already_exist/exec.c
@@ -0,0 +1,28 @@
+
+
+
+#include "../../../../../src/imports.h"
+#include "../../../../../src/declaration.h"
+
+#undef DATABASE_PATH
+#define DATABASE_PATH "tests/target/content"
+#undef TRANSACTION_PATH
+#define TRANSACTION_PATH  "tests/target/transactions"
+
+#include "../../../../../src/definition.h"
+
+
+int main(){
+    start_namespaces();
+    dtw_now = 0;
+
+    create_root_user_if_not_exist();
+    ApiBridge *bridge = newApiBridge();
+    ApiBridge_create_token(bridge,"root","root",-1);
+    ApiBridge_create_user(bridge,"test00000","test@gmail.com","test123456",false,false);
+    ApiBridge_create_user(bridge,"test00000","test5@gmail.com","test123456",false,false);
+
+    ApiBridge_represent(bridge);
+
+    ApiBridge_free(bridge);
+}
