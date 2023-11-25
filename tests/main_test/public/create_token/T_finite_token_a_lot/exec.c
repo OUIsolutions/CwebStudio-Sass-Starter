416440896a9845f09d032c8ee1cf995ee32d92f6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-25 20:05:50 -0300

    att

---
+++ tests/main_test/public/create_token/T_finite_token_a_lot/exec.c
@@ -0,0 +1,24 @@
+
+#include "../../../../../src/imports.h"
+#include "../../../../../src/declaration.h"
+
+#undef DATABASE_PATH
+#define DATABASE_PATH "tests/target/content"
+#undef TRANSACTION_PATH
+#define TRANSACTION_PATH  "tests/target/transactions"
+
+#include "../../../../../src/definition.h"
+
+
+int main(){
+    start_namespaces();
+    dtw_now = 0;
+
+    create_root_user_if_not_exist();
+    ApiBridge *bridge = newApiBridge();
+    for(int i = 0; i < 1000;i++){
+        ApiBridge_create_token(bridge,"root","root",30);
+    }
+    ApiBridge_represent(bridge);
+    ApiBridge_free(bridge);
+}
