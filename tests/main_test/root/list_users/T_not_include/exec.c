a8e29beb06609b139157463de2dfdbc8fe0c0c8f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-25 22:36:38 -0300

    at

---
+++ tests/main_test/root/list_users/T_not_include/exec.c
@@ -0,0 +1,31 @@
+
+
+
+#include "../../../../../src/imports.h"
+#include "../../../../../src/declaration.h"
+
+#undef DATABASE_PATH
+#define DATABASE_PATH "tests/target/content"
+#undef TRANSACTION_PATH
+#define TRANSACTION_PATH  "tests/target/transactions"
+
+#include "../../../../../src/definition.h"
+
+
+
+int main(){
+    start_namespaces();
+    dtw_now = 0;
+
+    create_root_user_if_not_exist();
+    ApiBridge *bridge = newApiBridge();
+    ApiBridge_create_token(bridge,"root","root",-1);
+    ApiBridge_create_user(bridge,"Mateus Moutinho","mateusmoutinho01@gmail.com","aaaaaaaaaaaaaaaaaaaaaa",false,false);
+    ApiBridge_create_user(bridge,"Mateus Moutinho","mateusmoutinho01@gmail.com","aaaaaaaaaaaaaaaaaaaaaa",false,false);
+    ApiBridge_create_user(bridge,"Rodrigo Moutinho","mateusmoutinho01@gmail.com","aaaaaaaaaaaaaaaaaaaaaa",false,false);
+    ApiBridge_list_users(bridge,NULL,false,false);
+
+    ApiBridge_represent(bridge);
+
+    ApiBridge_free(bridge);
+}
