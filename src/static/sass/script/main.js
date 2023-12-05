daaa2bb92deb76fae89816dfb61368a7b65fa29a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-05 15:31:01 -0300

    at

--- src/static/sass/script/main.js
+++ src/static/sass/script/main.js
@@ -4,15 +4,16 @@ function main(){
 
     let root_element = new Element404();
 
-    root_element.div({},"aaaaaaa");
+    root_element.div(null,()=>{
+        let page_state = root_element.getSimpleState(CURRENT_PAGE,LOGIN_PAGE);
 
-    let page_state = root_element.getSimpleState(CURRENT_PAGE,LOGIN_PAGE);
+        let page_value = page_state.getValue()
 
-    let page_value = page_state.getValue()
+        if(page_value === LOGIN_PAGE){
+            render_login_page(root_element)
+        }
+    });
 
-    if(page_value === LOGIN_PAGE){
-        render_login_page(root_element)
-    }
 
     root_element.render(document.getElementById("root"));
 
