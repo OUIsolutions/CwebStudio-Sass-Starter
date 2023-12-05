7378ef8e409c42eee9c60652feb773e54d4f6f34
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-05 16:56:15 -0300

    at

--- src/static/sass/script/main.js
+++ src/static/sass/script/main.js
@@ -8,11 +8,32 @@ function main(){
 
         this.p(null,"the value of state is: "+this.getPrimitiveState("num",0).getValue());
 
+        let input = this.getPrimitiveState('input','');
+
+        this.input({
+            value:input.getValue(),
+            focusout:(v)=> {
+                this.getPrimitiveState('input').setValue(v);
+                this.render();;
+            }
+     
+        })
+        if(input.getValue('input') === 'mateus'){
+            this.p(null,'aaaaaaaaaaaaaaaaaaaaa');
+
+        }
+
+
 
         this.button({click:()=>{
                 this.getPrimitiveState("num",0).increment(1);
                 this.render();
             }},"add")
+
+        this.button({click:()=>{
+                this.getPrimitiveState("num",0).decrement(1);
+                this.render();
+            }},"remove")
 
     }).render(document.getElementById("root"));
 
