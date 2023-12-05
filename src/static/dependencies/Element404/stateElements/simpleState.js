34db70fa70a120d2a5384e19514e179977b1da5e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-05 11:59:06 -0300

    at

--- src/static/dependencies/Element404/stateElements/simpleState.js
+++ src/static/dependencies/Element404/stateElements/simpleState.js
@@ -2,9 +2,10 @@
 
 class State{
     
-    constructor(element404,name,defaultValue){
+    constructor(element404,name,defaultValue,father){
         this.name = name
         this.element404 = element404
+        this.father = father
         if(!this.getValue()&& defaultValue !== undefined){
             this.setValue(defaultValue)
         }
@@ -15,13 +16,17 @@ class State{
         return  this.element404.state[this.name]
     }
 
+    getState(name,defaultValue){
+        return new State(this.element404,name,defaultValue,this)
+    }
+
+
+
     setValue(value){
 
-        if(value.value != undefined){
-            this.element404.state[this.name] = value.value
-            return
-        }
-        this.element404.state[this.name] = value
+        
+
+        this.element404.state[this.name] =private_Element404_convert_values(value)
     }
 
 
