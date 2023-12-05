9e2c097c148a676be0a36985ee8e23e89db44320
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-05 16:27:45 -0300

    at

---
+++ src/static/dependencies/Element404/stateElements/primitiveState.js
@@ -0,0 +1,15 @@
+
+
+function PrimitiveState (father, name, defaultValue) {
+
+    this.name = name
+    this.father = father
+
+    if (!this.getValue() && defaultValue !== undefined) {
+        this.setValue(defaultValue)
+    }
+}
+PrimitiveState.prototype.increment = function (value){
+    let old = this.getValue();
+    this.setValue(old+value)
+}
