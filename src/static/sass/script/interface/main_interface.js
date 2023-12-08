795dc0647f0483e8d28e3cd4f7618796ea583bb9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-07 21:33:00 -0300

    at

--- src/static/sass/script/interface/main_interface.js
+++ src/static/sass/script/interface/main_interface.js
@@ -6,9 +6,13 @@ function main_interface(){
 
    let connected = interface.getPrimitiveState(CONNECTED).getValue()
 
-   let interface_style = {}
+   let interface_style = {
+        position:'absolute',
+        width:'100vw',
+        height: '100vh'
+   }
    if(!connected){
-       interface_style['backgroundImage'] = `url("${BACKGROUND_IMAGE}")`;
+       interface_style['background'] = `url('${BACKGROUND_IMAGE}')`;
    }
 
    interface.div({style:interface_style},()=>{
