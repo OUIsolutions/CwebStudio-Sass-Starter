0af360b06f5a7a0f35832436e78476e685ca2d66
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-07 21:41:37 -0300

    at

--- src/static/sass/script/interface/main_interface.js
+++ src/static/sass/script/interface/main_interface.js
@@ -8,12 +8,17 @@ function main_interface(){
 
    let interface_style = {
         position:'absolute',
+        padding:'0',
         width:'100vw',
         height: '100vh'
    }
+   
    if(!connected){
        interface_style['background'] = `url('${BACKGROUND_IMAGE}')`;
+       interface_style['background-size'] = 'cover'
+       interface_style[' background-position'] = 'center center'
    }
+
 
    interface.div({style:interface_style},()=>{
 
