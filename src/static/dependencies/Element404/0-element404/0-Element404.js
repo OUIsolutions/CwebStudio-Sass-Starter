820b8d2efb4edcb84fbd11b1b0643ba35d5151a3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-09 00:06:19 -0300

    at

--- src/static/dependencies/Element404/0-element404/0-Element404.js
+++ src/static/dependencies/Element404/0-element404/0-Element404.js
@@ -1,10 +1,4 @@
 
-/**
- * @typedef Element404
- * @type {object}
- * @property {HTMLElement} root - the root element
- * @property {object} state - the state of the element
- */
  function Element404(generator){
         this.root = document.createDocumentFragment();
         this.generator = generator
--- src/static/dependencies/Element404/0-element404/3-state.js
+++ src/static/dependencies/Element404/0-element404/3-state.js
@@ -1,11 +1,20 @@
 
 
-
+/**
+ * Creates an Primitive state
+ * @param {string} name The name of the state
+ * @param {any} defaultValue The the default value
+ * @returns {PrimitiveState}
+ */
 Element404.prototype.getPrimitiveState = function(name,defaultValue){
-    return new PrimitiveState(this,name,defaultValue)
-
+    return new PrimitiveState(this,this,name,defaultValue)
 }
 
+/**
+ * Creates a Object state
+ * @param {string} name The name of the state
+ * @returns {ObjectState}
+ */
 Element404.prototype.getObjectState = function(name){
     return new ObjectState(this,this,name)
 
