fb0512caf23981acf6e78914ecea31957a9950aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-13 00:51:20 -0300

    at

--- src/static/sass/script/actions/efetuate_login.js
+++ src/static/sass/script/actions/efetuate_login.js
@@ -6,9 +6,10 @@
  */
  function efetuate_login(main_interface, main_state,token){
 
-    fetch(GET_SELF_PROPS,{headers:{token:token,include_tokens:true}})
+    fetch(GET_SELF_PROPS,{headers:{token:token}})
     .then(data => data.json())
     .then(data =>{
         console.log(data)
     })
+    
  }
