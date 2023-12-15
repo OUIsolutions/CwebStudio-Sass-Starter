29d2cfc1e0d4eb159c2297b8d7b1f1b9ac14e84a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-14 22:26:55 -0300

    at

--- src/static/sass/global_components/profile_picture/style.js
+++ src/static/sass/global_components/profile_picture/style.js
@@ -4,14 +4,16 @@ class  ProfilePictureStyle{
 
     profile_picture_div(){
         return {
+            'margin-top':'5vw',
+            'margin-left':'5vw',
             width:'10vw',
             height:'10vw'
         }
     }
     profile_picture_img(){
         return{
-            width:'10vw',
-            height:'10vw',
+            width:'100%',
+            height:'100%',
             background: `url('${assets['snow']}')`,
             'background-size': 'cover',
             ' background-position':'center center'
