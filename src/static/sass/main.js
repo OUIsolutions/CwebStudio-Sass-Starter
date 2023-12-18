06bc30d491afc252f8a2547652d866709a6e55de
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-18 12:01:49 -0300

    at

--- src/static/sass/main.js
+++ src/static/sass/main.js
@@ -3,6 +3,24 @@
 
 
 
+/**
+ * Creates an H3
+ * @param {MainState} main_state
+ */
+function create_main_interface_style(main_state){
+    let interface_style = {
+        position:'fixed',
+        width:'100vw',
+        height: '100vh'
+    }
+
+    if(main_state.page_start.current){
+        interface_style['background'] = `url('${assets['background']}')`;
+        interface_style['background-size'] = 'cover';
+        interface_style[' background-position'] = 'center center';
+    }
+    return interface_style;
+}
 
 
 
@@ -59,7 +77,7 @@ function main(){
     }
     
     if(token){
-        efetuate_login(main_interface, main_state, token)
+        efetuate_login(main_interface, main_state)
     }
 
 
