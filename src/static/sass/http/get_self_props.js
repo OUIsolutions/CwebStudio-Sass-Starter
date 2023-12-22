e179f2bb62d64a37ed5e7019d9381ff82ccf538d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-22 07:02:27 -0300

    att

--- src/static/sass/http/get_self_props.js
+++ src/static/sass/http/get_self_props.js
@@ -1,5 +1,12 @@
 
-
+/**
+ * @typedef {object} SelfProps
+ * @property {string} username
+ * @property {string} creation
+ * @property {string} last_update
+ * @property
+ * */
+{"user_props":{"username":"rootRR","creation":"2023-12-15 07:25:57","last_update":"2023-12-22 05:37:37","email":"root","verified":true,"root":true}}
 async  function get_self_props(){
 
     return await  make_autenticated_requisition(GET_SELF_PROPS_ROUTE);
