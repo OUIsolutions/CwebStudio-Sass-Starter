50d4ade61e7fd288361fb6affb877f4313c79e9b
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-02 21:50:01 -0300

    at

--- src/static/sass/main_interface/components/start_page/create_start_page.js
+++
@@ -1,5 +0,0 @@
-
-
-function  main_interface_create_start_page(){
-    
-}
--- src/static/sass/main_interface/main_interface_loop.js
+++ src/static/sass/main_interface/main_interface_loop.js
@@ -6,7 +6,9 @@ function main_interface_loop(){
     let token = main_interface.getStateValue("token");
     
     if(!token){
-        
+        start_page_component();
+        return;
     }
+    
 
 }
