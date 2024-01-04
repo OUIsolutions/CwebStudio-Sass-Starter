875946f2727c96baa08ab58f7821692a2d7171dc
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-04 17:15:12 -0300

    at

--- src/static/sass/interface/start_page/start_page.js
+++ src/static/sass/interface/start_page/start_page.js
@@ -5,13 +5,11 @@ function  render_start_page(main_interface){
         main_interface.div((sub_element)=>{
 
             sub_element.inline_style({
-                position:'fixed',
-                width:'100vw',
-                height: '100vh',
                 'background': `url('${assets['background']}')`,
                 'background-size':'cover',
                 'background-position': 'center center'
             });
+            sub_element.set_prop("APosition","$(0%,0%,100%,100%)")
 
             render_main_form_container(main_interface);
 
